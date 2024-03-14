fb4a8e553fa1d51e2560fd672586477e43d56423
parkdigy <parkdigy@gmail.com>
2024-03-14 23:11:12 +0900

    1.0.14

--- dist/DefaultLayout/SideMenuList/SideMenuList.style.d.ts
+++ dist/DefaultLayout/SideMenuList/SideMenuList.style.d.ts
@@ -1,11 +1,4 @@
 /// <reference types="react" />
-export declare const StyledList: import("@emotion/styled").StyledComponent<{
-    children?: import("react").ReactNode;
-    classes?: Partial<import("@mui/material").ListClasses> | undefined;
-    dense?: boolean | undefined;
-    disablePadding?: boolean | undefined;
-    subheader?: import("react").ReactNode;
-    sx?: import("@mui/material").SxProps<import("@mui/material").Theme> | undefined;
-} & import("@mui/material/OverridableComponent").CommonProps & Omit<Pick<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLUListElement>, HTMLUListElement>, "key" | keyof import("react").HTMLAttributes<HTMLUListElement>> & {
+export declare const StyledList: import("@emotion/styled").StyledComponent<import("@mui/material").ListOwnProps & import("@mui/material/OverridableComponent").CommonProps & Omit<Omit<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLUListElement>, HTMLUListElement>, "ref"> & {
     ref?: ((instance: HTMLUListElement | null) => void) | import("react").RefObject<HTMLUListElement> | null | undefined;
-}, keyof import("@mui/material/OverridableComponent").CommonProps | "children" | "dense" | "disablePadding" | "subheader" | "sx"> & import("@mui/system").MUIStyledCommonProps<import("@mui/material").Theme>, {}, {}>;
+}, "className" | "style" | "classes" | "children" | "dense" | "disablePadding" | "subheader" | "sx"> & import("@mui/system").MUIStyledCommonProps<import("@mui/material").Theme>, {}, {}>;
