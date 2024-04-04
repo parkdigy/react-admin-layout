6a8a775cf924e176b0075d02d688fa27e67d8eb3
박영철 <parkdigy@gmail.com>
2024-04-04 16:03:30 +0900

    1.0.21

--- examples/src/router/index.tsx
+++ examples/src/router/index.tsx
@@ -8,12 +8,20 @@ const routes = () => {
     <Routes>
       <Route path='/' element={<Dashboard />} />
       <Route path='/menu_1' element={<Menu1 />} />
-      <Route path='/menu_2/sub_1' element={<Menu2Sub1 />} />
-      <Route path='/menu_2/sub_2' element={<Menu2Sub2 />} />
-      <Route path='/menu_2/sub_3' element={<Menu2Sub3 />} />
-      <Route path='/menu_3/sub_1' element={<Menu3Sub1 />} />
-      <Route path='/menu_3/sub_2' element={<Menu3Sub2 />} />
-      <Route path='/menu_3/sub_3' element={<Menu3Sub3 />} />
+      <Route
+        path='/menu_2/*'
+        element={
+          <Routes>
+            <Route path='/sub_1' element={<Menu2Sub1 />} />
+            <Route path='/sub_2' element={<Menu2Sub2 />} />
+            <Route path='/sub_3' element={<Menu2Sub3 />} />
+            <Route path='/sub_1' element={<Menu3Sub1 />} />
+            <Route path='/sub_2' element={<Menu3Sub2 />} />
+            <Route path='/sub_3' element={<Menu3Sub3 />} />
+            <Route path='*' element={<Navigate to='/' />} />
+          </Routes>
+        }
+      />
       <Route path='*' element={<Navigate to='/' />} />
     </Routes>
   );
